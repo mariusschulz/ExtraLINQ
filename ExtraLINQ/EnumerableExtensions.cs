4000b5b8403248c23eb11617a73ba0b0224e0f83
Marius Schulz <schulz.marius@t-online.de>
2011-08-02 21:42:14 +0200

    commented the class, removed an unused namespace and changed the type parameter's name

--- ExtraLINQ/EnumerableExtensions.cs
+++ ExtraLINQ/EnumerableExtensions.cs
@@ -1,13 +1,24 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ExtraLINQ
 {
+    /// <summary>
+    /// Provides handy extension methods for collections.
+    /// </summary>
     public static class EnumerableExtensions
     {
-        public static bool IsEmpty<T>(this IEnumerable<T> source)
+        /// <summary>
+        /// Determines whether the specified collection is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> to check for emptiness.</param>
+        /// <returns>
+        ///   <c>true</c> if <paramref name="source"/> is empty; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
+        public static bool IsEmpty<TSource>(this IEnumerable<TSource> source)
         {
             if (source == null)
             {
@@ -17,6 +28,20 @@ namespace ExtraLINQ
             return !source.Any();
         }
 
+        /// <summary>
+        /// Determines whether none of the elements of a collection satisfy a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> to check for matches.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>
+        ///   <c>true</c> if no element satisfies the specified condition; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        ///   <para><paramref name="source"/> is null.</para>
+        ///   <para>- or - </para>
+        ///   <para><paramref name="predicate"/> is null.</para>
+        /// </exception>
         public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
             if (source == null)
