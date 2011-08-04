c9e85760ac5e178f0499e247e8ed32f443ead228
Marius Schulz <schulz.marius@t-online.de>
2011-08-04 15:36:56 +0200

    replaced fully qualified types in XML comments by common type name

--- ExtraLINQ/EnumerableExtensions.cs
+++ ExtraLINQ/EnumerableExtensions.cs
@@ -13,12 +13,12 @@ namespace ExtraLINQ
         /// Determines whether the specified collection contains exactly the specified number of items.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> to count.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> to count.</param>
         /// <param name="expectedItemCount">The number of items the specified collection is expected to contain.</param>
         /// <returns>
         ///   <c>true</c> if <paramref name="source"/> contains exactly <paramref name="expectedItemCount"/> items; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
         public static bool CountsExactly<TSource>(this IEnumerable<TSource> source, int expectedItemCount)
         {
             if (source == null)
@@ -38,7 +38,7 @@ namespace ExtraLINQ
         /// Determines whether the specified collection contains exactly the specified number of items satisfying the specified condition.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> to count satisfying items.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> to count satisfying items.</param>
         /// <param name="expectedItemCount">The number of satisfying items the specified collection is expected to contain.</param>
         /// <param name="predicate">A function to test each element for a condition.</param>
         /// <returns>
@@ -74,13 +74,13 @@ namespace ExtraLINQ
         /// Determines whether the specified collection's item count is equal to or lower than <paramref name="expectedMaxItemCount"/>.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> whose items to count.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> whose items to count.</param>
         /// <param name="expectedMaxItemCount">The maximum number of items the specified collection is expected to contain.</param>
         /// <returns>
         ///   <c>true</c> if the item count of <paramref name="source"/> is equal to or lower than <paramref name="expectedMaxItemCount"/>; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="System.ArgumentException">Thrown when <paramref name="expectedMaxItemCount"/> is negative.</exception>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="expectedMaxItemCount"/> is negative.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
         public static bool CountsMax<TSource>(this IEnumerable<TSource> source, int expectedMaxItemCount)
         {
             if (source == null)
@@ -100,13 +100,13 @@ namespace ExtraLINQ
         /// Determines whether the specified collection contains exactly <paramref name="expectedMaxItemCount"/> or less items satisfying a condition.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> whose items to count.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> whose items to count.</param>
         /// <param name="expectedMaxItemCount">The maximum number of items satisfying the specified condition the specified collection is expected to contain.</param>
         /// <param name="predicate">A function to test each element for a condition.</param>
         /// <returns>
         ///   <c>true</c> if the item count of satisfying items is equal to or less than <paramref name="expectedMaxItemCount"/>; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="System.ArgumentNullException">
+        /// <exception cref="ArgumentNullException">
         ///   <para><paramref name="source"/> is null.</para>
         ///   <para>- or - </para>
         ///   <para><paramref name="predicate"/> is null.</para>
@@ -130,12 +130,12 @@ namespace ExtraLINQ
         /// Determines whether the specified collection's item count is equal to or greater than <paramref name="expectedMinItemCount"/>.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> whose items to count.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> whose items to count.</param>
         /// <param name="expectedMinItemCount">The minimum number of items the specified collection is expected to contain.</param>
         /// <returns>
         ///   <c>true</c> if the item count of <paramref name="source"/> is equal to or greater than <paramref name="expectedMinItemCount"/>; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
         /// <remarks>
         /// No exception is thrown in case a negative <paramref name="expectedMinItemCount"/> is passed.
         /// </remarks>
@@ -153,13 +153,13 @@ namespace ExtraLINQ
         /// Determines whether the specified collection contains exactly <paramref name="expectedMinItemCount"/> or more items satisfying a condition.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> whose items to count.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> whose items to count.</param>
         /// <param name="expectedMinItemCount">The minimum number of items satisfying the specified condition the specified collection is expected to contain.</param>
         /// <param name="predicate">A function to test each element for a condition.</param>
         /// <returns>
         ///   <c>true</c> if the item count of satisfying items is equal to or greater than <paramref name="expectedMinItemCount"/>; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="System.ArgumentNullException">
+        /// <exception cref="ArgumentNullException">
         ///   <para><paramref name="source"/> is null.</para>
         ///   <para>- or - </para>
         ///   <para><paramref name="predicate"/> is null.</para>
@@ -186,12 +186,12 @@ namespace ExtraLINQ
         /// Returns the item at a specified index in a specified collection using a specified indexing strategy.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/>containing the item.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/>containing the item.</param>
         /// <param name="index">The item's index.</param>
         /// <param name="indexingStrategy">The <see cref="IndexingStrategy"/> to use.</param>
         /// <returns>The item at the specified index.</returns>
-        /// <exception cref="System.ArgumentException">Thrown when <paramref name="source"/> is empty.</exception>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="source"/> is empty.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
         public static TSource ElementAt<TSource>(this IEnumerable<TSource> source, int index, IndexingStrategy indexingStrategy)
         {
             if (source == null)
@@ -230,11 +230,11 @@ namespace ExtraLINQ
         /// Determines whether the specified collection is empty.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> to check for emptiness.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> to check for emptiness.</param>
         /// <returns>
         ///   <c>true</c> if <paramref name="source"/> is empty; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
         public static bool IsEmpty<TSource>(this IEnumerable<TSource> source)
         {
             if (source == null)
@@ -249,7 +249,7 @@ namespace ExtraLINQ
         /// Determines whether the specified collection is null or empty.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> to check for null or emptiness.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> to check for null or emptiness.</param>
         /// <returns>
         ///   <c>true</c> if <paramref name="source"/> is null or empty; otherwise, <c>false</c>.
         /// </returns>
@@ -267,12 +267,12 @@ namespace ExtraLINQ
         /// Determines whether none of the elements of a collection satisfy a condition.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of source.</typeparam>
-        /// <param name="source">The <see cref="System.Collections.Generic.IEnumerable{TSource}"/> to check for matches.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> to check for matches.</param>
         /// <param name="predicate">A function to test each element for a condition.</param>
         /// <returns>
         ///   <c>true</c> if no element satisfies the specified condition; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="System.ArgumentNullException">
+        /// <exception cref="ArgumentNullException">
         ///   <para><paramref name="source"/> is null.</para>
         ///   <para>- or - </para>
         ///   <para><paramref name="predicate"/> is null.</para>
