1aa4cef37c5d161316e5cc376831cbf42886d672
Marius Schulz <schulz.marius@t-online.de>
2011-08-07 17:19:22 +0200

    renamed "Except" to "Without" and changed functionality

--- ExtraLINQ/EnumerableExtensions.cs
+++ ExtraLINQ/EnumerableExtensions.cs
@@ -227,43 +227,41 @@ namespace ExtraLINQ
         }
 
         /// <summary>
-        /// Produces the set difference of the specified collection and the single item.
+        /// Returns all elements of <paramref name="source"/> except <paramref name="item"/>.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
         /// <param name="source">The <see cref="IEnumerable{TSource}"/>containing the item.</param>
         /// <param name="item">The item to remove.</param>
         /// <returns>
-        /// The set difference of the specified collection and the single item.
+        /// All elements of <paramref name="source"/> except <paramref name="item"/>.
         /// </returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
-        public static IEnumerable<TSource> Except<TSource>(this IEnumerable<TSource> source, TSource item)
+        public static IEnumerable<TSource> Without<TSource>(this IEnumerable<TSource> source, TSource item)
         {
             if (source == null)
             {
                 throw new ArgumentNullException("source");
             }
 
-            TSource[] singleItemArray = new[] { item };
-
-            return source.Except(singleItemArray);
+            return source.Where(element => !element.Equals(item));
         }
 
         /// <summary>
-        /// Produces the set difference of the specified collection and the single item using the specified equality comparer to compare values.
+        /// Returns all elements of <paramref name="source"/> except <paramref name="item"/> using the specified equality comparer to compare values.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The <see cref="IEnumerable{TSource}"/>containing the item.</param>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> containing the item.</param>
         /// <param name="item">The item to remove.</param>
         /// <param name="equalityComparer">The equality comparer to use.</param>
         /// <returns>
-        /// The set difference of the specified collection and the single item.
+        /// All elements of <paramref name="source"/> except <paramref name="item"/>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
         ///   <para><paramref name="source"/> is null.</para>
         ///   <para>- or - </para>
         ///   <para><paramref name="equalityComparer"/> is null.</para>
         ///   </exception>
-        public static IEnumerable<TSource> Except<TSource>(this IEnumerable<TSource> source, TSource item,
+        public static IEnumerable<TSource> Without<TSource>(this IEnumerable<TSource> source, TSource item,
             IEqualityComparer<TSource> equalityComparer)
         {
             if (source == null)
@@ -276,9 +274,7 @@ namespace ExtraLINQ
                 throw new ArgumentNullException("equalityComparer");
             }
 
-            TSource[] singleItemArray = new[] { item };
-
-            return source.Except(singleItemArray, equalityComparer);
+            return source.Where(element => !equalityComparer.Equals(element, item));
         }
 
         /// <summary>
