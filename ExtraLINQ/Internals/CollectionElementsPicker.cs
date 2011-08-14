f987e853dbd158cd9794e83de435b3a8ef1329dc
Marius Schulz <schulz.marius@t-online.de>
2011-08-14 03:28:46 +0200

    renamed methods returning random collection elements

--- ExtraLINQ/Internals/CollectionElementsPicker.cs
+++ ExtraLINQ/Internals/CollectionElementsPicker.cs
@@ -15,18 +15,27 @@ namespace ExtraLINQ.Internals
             _randomNumberGenerator = new Random();
         }
 
-        public TSource PickRandomItem()
+        public TSource PickRandomElement()
         {
-            return PickRandomItem(_randomNumberGenerator);
+            return PickRandomElement(_randomNumberGenerator);
         }
 
-        public TSource PickRandomItem(Random randomNumberGenerator)
+        public TSource PickRandomElement(Random randomNumberGenerator)
         {
             int itemCount = _source.Count();
             int randomIndex = randomNumberGenerator.Next(itemCount);
             TSource randomItem = _source.ElementAt(randomIndex);
 
             return randomItem;
+        }
+
+        public IEnumerable<TSource> PickRandomElements(int randomElementsCount)
+        {
+            CollectionShuffler<TSource> shuffler = new CollectionShuffler<TSource>(_source);
+            IEnumerable<TSource> shuffledCollection = shuffler.ShuffleCollection();
+            IEnumerable<TSource> randomElements = shuffledCollection.Take(randomElementsCount);
+
+            return randomElements;
         }
     }
 }
