a598ca52059c4c207caaa75ae299669fe4a6ea52
Marius Schulz <schulz.marius@t-online.de>
2011-08-14 04:09:34 +0200

    implemented "Shuffle" extension method accepting a random number generator
    completed XML documentation

--- ExtraLINQ/EnumerableExtensions.cs
+++ ExtraLINQ/EnumerableExtensions.cs
@@ -437,6 +437,7 @@ namespace ExtraLINQ
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
         /// <param name="source">The <see cref="IEnumerable{TSource}"/> to return an element from.</param>
         /// <returns>A shuffled collection containing the elements of <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
         public static IEnumerable<TSource> Shuffle<TSource>(this IEnumerable<TSource> source)
         {
             if (source == null)
@@ -446,6 +447,36 @@ namespace ExtraLINQ
 
             CollectionShuffler<TSource> shuffler = new CollectionShuffler<TSource>(source);
             IEnumerable<TSource> shuffledCollection = shuffler.ShuffleCollection();
+
+            return shuffledCollection;
+        }
+
+        /// <summary>
+        /// Shuffles the specified source using the specified random number generator.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> to shuffle.</param>
+        /// <param name="randomNumberGenerator">The random number generator used to shuffle the specified collection.</param>
+        /// <returns>A shuffled collection containing the elements of <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <para><paramref name="source"/> is null.</para>
+        ///   <para>- or - </para>
+        ///   <para><paramref name="randomNumberGenerator"/> is null.</para>
+        ///   </exception>
+        public static IEnumerable<TSource> Shuffle<TSource>(this IEnumerable<TSource> source, Random randomNumberGenerator)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (randomNumberGenerator == null)
+            {
+                throw new ArgumentNullException("randomNumberGenerator");
+            }
+
+            CollectionShuffler<TSource> shuffler = new CollectionShuffler<TSource>(source);
+            IEnumerable<TSource> shuffledCollection = shuffler.ShuffleCollection(randomNumberGenerator);
 
             return shuffledCollection;
         }
