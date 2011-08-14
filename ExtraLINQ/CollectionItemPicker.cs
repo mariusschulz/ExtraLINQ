e3965f18b56e63a26ce5fee09bc6c4ec1122ed6f
Marius Schulz <schulz.marius@t-online.de>
2011-08-14 02:19:25 +0200

    implemented "Random" extension method

---
+++ ExtraLINQ/CollectionItemPicker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtraLINQ
+{
+    internal class CollectionItemPicker<TSource>
+    {
+        private readonly Random _randomNumberGenerator;
+        private readonly IEnumerable<TSource> _source;
+
+        public CollectionItemPicker(IEnumerable<TSource> source)
+        {
+            _source = source;
+            _randomNumberGenerator = new Random();
+        }
+
+        public TSource PickRandomItem()
+        {
+            int itemCount = _source.Count();
+            int randomIndex = _randomNumberGenerator.Next(itemCount);
+            TSource randomItem = _source.ElementAt(randomIndex);
+
+            return randomItem;
+        }
+    }
+}
