c1f57efd9d1046a408e1270eb4f41cf0e1770856
Marius Schulz <schulz.marius@t-online.de>
2011-08-14 03:50:48 +0200

    implemented another "Random" overload
    clarified comment

--- ExtraLINQ/EnumerableExtensions.cs
+++ ExtraLINQ/EnumerableExtensions.cs
@@ -335,11 +335,11 @@ namespace ExtraLINQ
                 throw new ArgumentNullException("source");
             }
 
-            // Create array from source to avoid multiple enumeration
+            // Create array from source for further use to avoid multiple enumeration
             TSource[] sourceArray = source.ToArray();
 
             bool randomElementsIsOutOfRange = randomElementsCount < 0
-                || randomElementsCount > sourceArray.Length;
+                                              || randomElementsCount > sourceArray.Length;
 
             if (randomElementsIsOutOfRange)
             {
@@ -348,6 +348,53 @@ namespace ExtraLINQ
 
             CollectionElementsPicker<TSource> elementsPicker = new CollectionElementsPicker<TSource>(sourceArray);
             IEnumerable<TSource> randomElements = elementsPicker.PickRandomElements(randomElementsCount);
+
+            return randomElements;
+        }
+
+        /// <summary>
+        /// Returns the specified number of random elements from <paramref name="source"/> using the specified random number generator.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The <see cref="IEnumerable{TSource}"/> to return an element from.</param>
+        /// <param name="randomElementsCount">The number of random elements to return.</param>
+        /// <param name="randomNumberGenerator">The random number generator used to select random elements.</param>
+        /// <returns>
+        /// A collection of random elements from <paramref name="source"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <para><paramref name="source"/> is null.</para>
+        ///   <para>- or - </para>
+        ///   <para><paramref name="randomNumberGenerator"/> is null.</para>
+        ///   </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when <paramref name="randomElementsCount"/> is negative or greater than the collection's element count.
+        /// </exception>
+        public static IEnumerable<TSource> Random<TSource>(this IEnumerable<TSource> source, int randomElementsCount, Random randomNumberGenerator)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (randomNumberGenerator == null)
+            {
+                throw new ArgumentNullException("randomNumberGenerator");
+            }
+
+            // Create array from source for further use to avoid multiple enumeration
+            TSource[] sourceArray = source.ToArray();
+
+            bool randomElementsIsOutOfRange = randomElementsCount < 0
+                                              || randomElementsCount > sourceArray.Length;
+
+            if (randomElementsIsOutOfRange)
+            {
+                throw new ArgumentOutOfRangeException("randomElementsCount");
+            }
+
+            CollectionElementsPicker<TSource> elementsPicker = new CollectionElementsPicker<TSource>(sourceArray);
+            IEnumerable<TSource> randomElements = elementsPicker.PickRandomElements(randomElementsCount, randomNumberGenerator);
 
             return randomElements;
         }
