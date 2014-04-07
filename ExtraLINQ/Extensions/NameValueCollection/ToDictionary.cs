f2de82d6ed06f41592e5d585ff17cb78fa6111ec
Marius Schulz <marius.schulz@me.com>
2014-04-08 00:16:52 +0200

    Implemented NameValueCollection.ToDictionary

---
+++ ExtraLINQ/Extensions/NameValueCollection/ToDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace ExtraLinq
+{
+    public static class NameValueCollectionExtensions
+    {
+        public static Dictionary<string, string> ToDictionary(this NameValueCollection collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("collection");
+            }
+
+            var dictionary = new Dictionary<string, string>(collection.Count);
+
+            foreach (string key in collection.Keys)
+            {
+                dictionary.Add(key, collection[key]);
+            }
+
+            return dictionary;
+        }
+    }
+}
