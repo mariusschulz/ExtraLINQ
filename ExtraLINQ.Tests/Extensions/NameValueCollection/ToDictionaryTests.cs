1e2e341631e24aaee516ec7c872beae1729d74ff
Marius Schulz <marius.schulz@me.com>
2014-04-08 00:16:38 +0200

    Wrote test for NameValueCollection.ToDictionary

---
+++ ExtraLINQ.Tests/Extensions/NameValueCollection/ToDictionaryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using ExtraLinq;
+using NUnit.Framework;
+
+namespace ExtraLINQ.Tests
+{
+    [TestFixture]
+    public class ToDictionaryTests
+    {
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Test]
+        public void ThrowsArgumentNullExceptionWhenCollectionIsNull()
+        {
+            NameValueCollection collection = null;
+            Dictionary<string, string> dictionary = collection.ToDictionary();
+        }
+    }
+}
