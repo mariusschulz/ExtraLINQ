a49bd26cb2206d27b968ec452d40403db7cc9f8b
Marius Schulz <marius.schulz@me.com>
2014-04-12 19:45:40 +0200

    Simplified the "None" extension method

--- ExtraLINQ/Extensions/IEnumerable/None.cs
+++ ExtraLINQ/Extensions/IEnumerable/None.cs
@@ -11,27 +11,12 @@ namespace ExtraLinq
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
         /// <param name="source">The <see cref="IEnumerable{TSource}"/> to check for matches.</param>
-        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="predicate">The predicate to check each element against.</param>
         /// <returns>
         ///   <c>true</c> if no element satisfies the specified condition; otherwise, <c>false</c>.
         /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///   <para><paramref name="source"/> is null.</para>
-        ///   <para>- or - </para>
-        ///   <para><paramref name="predicate"/> is null.</para>
-        /// </exception>
         public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
-            if (source == null)
-            {
-                throw new ArgumentNullException("source");
-            }
-
-            if (predicate == null)
-            {
-                throw new ArgumentNullException("predicate");
-            }
-
             return !source.Any(predicate);
         }
     }
