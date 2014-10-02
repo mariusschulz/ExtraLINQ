c345524e4f2fa343ec3cde334d0a5f8ec2e64a15
Marius Schulz <marius.schulz@me.com>
2014-10-03 00:52:25 +0200

    Moved files and deleted "ElementAt(..., IndexingStrategy)" method because of its potential to cause confusion

---
+++ src/app/ExtraLINQ/Extensions/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtraLinq.Extensions
+{
+    internal static class IntExtensions
+    {
+        public static int ClampTo(this int value, int lower, int upper)
+        {
+            int actualLower = Math.Min(lower, upper);
+            int actualUpper = Math.Max(lower, upper);
+
+            if (value < actualLower)
+            {
+                return actualLower;
+            }
+
+            if (value > actualUpper)
+            {
+                return actualUpper;
+            }
+
+            return value;
+        }
+    }
+}
