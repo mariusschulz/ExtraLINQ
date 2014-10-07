11ed2f365188cec471c4665f69e56b621ac0cd6d
Marius Schulz <marius.schulz@me.com>
2014-10-08 01:19:16 +0200

    Implemented "StringJoin"

---
+++ src/app/ExtraLINQ/IEnumerable/StringJoin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ExtraLinq
+{
+    public static partial class EnumerableExtensions
+    {
+        /// <summary>
+        /// Concatenates all items of a sequence using the specified separator between each item.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="values"/>.</typeparam>
+        /// <param name="values">A sequence that contains the objects to concatenate.</param>
+        /// <param name="separator">The string to use as a separator.</param>
+        /// <returns>A string holding the concatenated values.</returns>
+        public static string StringJoin<TSource>(this IEnumerable<TSource> values, string separator = "")
+        {
+            ThrowIf.Argument.IsNull(values, "values");
+            ThrowIf.Argument.IsNull(separator, "separator");
+
+            return string.Join(separator, values);
+        }
+    }
+}
---
+++ src/tests/ExtraLINQ.Tests/IEnumerable/StringJoinTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ExtraLinq.Tests
+{
+    public class StringJoinTests
+    {
+        [Fact]
+        public void EagerlyThrowsArgumentNullExceptionWhenCollectionIsNull()
+        {
+            IEnumerable<char> nullCollection = null;
+
+            Assert.Throws<ArgumentNullException>(() => nullCollection.StringJoin());
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenSeparatorIsNull()
+        {
+            string[] items = { "one", "two", "three" };
+
+            Assert.Throws<ArgumentNullException>(() => items.StringJoin(null));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(",")]
+        [InlineData("    ")]
+        public void ReturnsAnEmptyStringForAnEmptySequence(string separator)
+        {
+            string[] strings = new string[0];
+
+            string joined = strings.StringJoin(separator);
+
+            joined.Should().Be(string.Empty);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(",")]
+        [InlineData("    ")]
+        public void ReturnsTheSingleItemForASequenceWithJustOneItem(string separator)
+        {
+            string[] strings = { "The One Ring" };
+
+            string joined = strings.StringJoin(separator);
+
+            joined.Should().Be("The One Ring");
+        }
+
+        [Fact]
+        public void ReturnsAStringContainingAllItemsJoinedByTheGivenSeparator()
+        {
+            string[] strings = { "The", "One", "Ring" };
+
+            string joined = strings.StringJoin(" ");
+
+            joined.Should().Be("The One Ring");
+        }
+    }
+}
