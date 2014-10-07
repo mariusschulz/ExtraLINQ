29c000543858e1fcadf90ffde79cf04a61d2bd9e
Marius Schulz <marius.schulz@me.com>
2014-10-08 00:51:56 +0200

    First working version of "WhereNot"

---
+++ src/app/ExtraLINQ/IEnumerable/WhereNot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtraLinq
+{
+    public static partial class EnumerableExtensions
+    {
+        /// <summary>
+        /// Filters a sequence of values based on a predicate
+        /// and returns those values that don't match the given predicate.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to filter.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>Those values that don't match the given predicate.</returns>
+        public static IEnumerable<TSource> WhereNot<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            ThrowIf.Argument.IsNull(source, "source");
+            ThrowIf.Argument.IsNull(predicate, "predicate");
+
+            return source.Where(item => !predicate(item));
+        }
+    }
+}
---
+++ src/tests/ExtraLINQ.Tests/IEnumerable/WhereNotTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtraLinq.Tests
+{
+    public class WhereNotTests
+    {
+        [Fact]
+        public void EagerlyThrowsArgumentNullExceptionWhenCollectionIsNull()
+        {
+            IEnumerable<char> nullCollection = null;
+
+            Assert.Throws<ArgumentNullException>(() => nullCollection.WhereNot(_ => true));
+        }
+
+        [Fact]
+        public void EagerlyThrowsArgumentNullExceptionWhenPredicateIsNull()
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            Assert.Throws<ArgumentNullException>(() => numbers.WhereNot(null));
+        }
+
+        [Fact]
+        public void ReturnsAllItemsNotMatchingTheSpecifiedPredicate()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            int[] expectedOdds = { 1, 3, 5 };
+
+            numbers.WhereNot(n => n % 2 == 0).Should().Equal(expectedOdds);
+        }
+    }
+}
