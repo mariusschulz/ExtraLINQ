d14c64973de3c90a872b7fcdcd153dcd7fa72628
Marius Schulz <marius.schulz@me.com>
2014-10-09 23:53:57 +0200

    Wrote tests and implementation for "Cycle" method

---
+++ src/app/ExtraLINQ/IEnumerable/Cycle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtraLinq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static IEnumerable<TSource> Cycle<TSource>(this IEnumerable<TSource> source)
+        {
+            ThrowIf.Argument.IsNull(source, "source");
+
+            return EnumerateForever(source);
+        }
+
+        private static IEnumerable<TSource> EnumerateForever<TSource>(IEnumerable<TSource> source)
+        {
+            var enumeratedItems = new List<TSource>();
+
+            foreach (TSource item in source)
+            {
+                yield return item;
+                enumeratedItems.Add(item);
+            }
+
+            if (!enumeratedItems.Any())
+            {
+                yield break;
+            }
+
+            int index = 0;
+            while (true)
+            {
+                yield return enumeratedItems[index];
+                index = (index + 1) % enumeratedItems.Count;
+            }
+        }
+    }
+}
---
+++ src/tests/ExtraLINQ.Tests/IEnumerable/CycleTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtraLinq.Tests
+{
+    public class CycleTests
+    {
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenCollectionIsNull()
+        {
+            IEnumerable<object> nullCollection = null;
+
+            Assert.Throws<ArgumentNullException>(() => nullCollection.Cycle());
+        }
+
+        [Fact]
+        public void ReturnsAnEmptySequenceWhenPassedAnEmptySequence()
+        {
+            int[] numbers = new int[0];
+
+            int[] cycledNumbers = numbers.Cycle().ToArray();
+
+            cycledNumbers.Should().HaveCount(0);
+        }
+
+        [Fact]
+        public void CyclesThroughASequenceWithASingleItem()
+        {
+            int[] singleTen = { 10 };
+
+            int[] threeTens = singleTen.Cycle().Take(3).ToArray();
+
+            threeTens.Should().Equal(new[] { 10, 10, 10 });
+        }
+
+        [Fact]
+        public void CyclesThroughASequenceWithMultipleItems()
+        {
+            int[] bits = { 0, 1 };
+
+            int[] alternatingBits = bits.Cycle().Take(5).ToArray();
+
+            alternatingBits.Should().Equal(new[] { 0, 1, 0, 1, 0 });
+        }
+    }
+}
