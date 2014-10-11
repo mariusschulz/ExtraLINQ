f7294a3a9690ff05ec561a64fd5dd76e24927217
Marius Schulz <marius.schulz@me.com>
2014-10-12 01:47:13 +0200

    Added XML documentation for "TakeEvery"

--- src/app/ExtraLINQ/IEnumerable/TakeEvery.cs
+++ src/app/ExtraLINQ/IEnumerable/TakeEvery.cs
@@ -5,6 +5,14 @@ namespace ExtraLinq
 {
     public static partial class EnumerableExtensions
     {
+        /// <summary>
+        /// Returns every N-th element of a sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The sequence.</param>
+        /// <param name="step">Specifies N, that is, the number of elements to encounter before returning the current one.</param>
+        /// <remarks>The first item of the sequence will be returned, and after that all items offset by a multiple of step.</remarks>
+        /// <returns>Every N-th element of the given sequence.</returns>
         public static IEnumerable<TSource> TakeEvery<TSource>(this IEnumerable<TSource> source, int step)
         {
             ThrowIf.Argument.IsNull(source, "source");
