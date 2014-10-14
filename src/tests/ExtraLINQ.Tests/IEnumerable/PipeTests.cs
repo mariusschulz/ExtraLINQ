0ca677671db22f28e35d252c02c934bb64554bc9
Marius Schulz <marius.schulz@me.com>
2014-10-15 00:25:46 +0200

    Added overload of "Pipe" that accepts the index of each element

--- src/tests/ExtraLINQ.Tests/IEnumerable/PipeTests.cs
+++ src/tests/ExtraLINQ.Tests/IEnumerable/PipeTests.cs
@@ -30,12 +30,12 @@ namespace ExtraLinq.Tests
         public void CallsTheSpecifiedActionForEachElementWhenEnumerated()
         {
             int[] numbers = { 1, 2, 3 };
-            var passedArguments = new List<int>();
-            Action<int> action = passedArguments.Add;
+            var elementsPassedToAction = new List<int>();
+            Action<int> action = elementsPassedToAction.Add;
 
             numbers.Pipe(action).ToList();
 
-            passedArguments.Should().Equal(numbers);
+            elementsPassedToAction.Should().Equal(numbers);
         }
 
         [Fact]
@@ -43,6 +43,53 @@ namespace ExtraLinq.Tests
         {
             int[] numbers = { 1, 2, 3 };
             Action<int> action = Console.WriteLine;
+
+            IEnumerable<int> returnedSequence = numbers.Pipe(action);
+
+            returnedSequence.Should().Equal(numbers);
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenCollectionIsNullForActionWithIndex()
+        {
+            IEnumerable<object> nullCollection = null;
+            Action<object, int> doNothing = (element, index) => { };
+
+            Assert.Throws<ArgumentNullException>(() => nullCollection.Pipe(doNothing));
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenActionWithIndexIsNull()
+        {
+            int[] numbers = { 1, 2, 3 };
+            Action<int, int> nullAction = null;
+
+            Assert.Throws<ArgumentNullException>(() => numbers.Pipe(nullAction));
+        }
+
+        [Fact]
+        public void CallsTheSpecifiedActionWithIndexForEachElementWhenEnumerated()
+        {
+            int[] numbers = { 3, 4, 5 };
+            var elementsPassedToAction = new List<int>();
+            var indicesPassedToAction = new List<int>();
+            Action<int, int> action = (element, index) =>
+            {
+                elementsPassedToAction.Add(element);
+                indicesPassedToAction.Add(index);
+            };
+
+            numbers.Pipe(action).ToList();
+
+            elementsPassedToAction.Should().Equal(numbers);
+            indicesPassedToAction.Should().Equal(new[] { 0, 1, 2 });
+        }
+
+        [Fact]
+        public void ReturnsAllElementsOfTheSourceSequenceWithoutModificationForActionWithIndex()
+        {
+            int[] numbers = { 1, 2, 3 };
+            Action<int, int> action = (element, index) => Console.WriteLine(element);
 
             IEnumerable<int> returnedSequence = numbers.Pipe(action);
 
