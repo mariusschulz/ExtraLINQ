2ae59566e071a255812232f4a7a18348d1904f9b
Marius Schulz <marius.schulz@me.com>
2014-10-15 00:14:36 +0200

    Implemented "Pipe" method

---
+++ src/app/ExtraLINQ/IEnumerable/Pipe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtraLinq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static IEnumerable<TSource> Pipe<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            ThrowIf.Argument.IsNull(source, "source");
+            ThrowIf.Argument.IsNull(action, "action");
+
+            return PipeImplementation(source, action);
+        }
+
+        private static IEnumerable<TSource> PipeImplementation<TSource>(IEnumerable<TSource> source, Action<TSource> action)
+        {
+            foreach (var element in source)
+            {
+                action(element);
+                yield return element;
+            }
+        }
+    }
+}
---
+++ src/tests/ExtraLINQ.Tests/IEnumerable/PipeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtraLinq.Tests
+{
+    public class PipeTests
+    {
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenCollectionIsNull()
+        {
+            IEnumerable<object> nullCollection = null;
+            Action<object> doNothing = _ => { };
+
+            Assert.Throws<ArgumentNullException>(() => nullCollection.Pipe(doNothing));
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenActionIsNull()
+        {
+            int[] numbers = { 1, 2, 3 };
+            Action<int> action = null;
+
+            Assert.Throws<ArgumentNullException>(() => numbers.Pipe(action));
+        }
+
+        [Fact]
+        public void CallsTheSpecifiedActionForEachElementWhenEnumerated()
+        {
+            int[] numbers = { 1, 2, 3 };
+            var passedArguments = new List<int>();
+            Action<int> action = passedArguments.Add;
+
+            numbers.Pipe(action).ToList();
+
+            passedArguments.Should().Equal(numbers);
+        }
+
+        [Fact]
+        public void ReturnsAllElementsOfTheSourceSequenceWithoutModification()
+        {
+            int[] numbers = { 1, 2, 3 };
+            Action<int> action = Console.WriteLine;
+
+            IEnumerable<int> returnedSequence = numbers.Pipe(action);
+
+            returnedSequence.Should().Equal(numbers);
+        }
+    }
+}
