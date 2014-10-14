f486d99f30df2e78997aab159fc13709a8f9b76b
Marius Schulz <marius.schulz@me.com>
2014-10-14 23:57:01 +0200

    Added XML documentation

--- src/app/ExtraLINQ/IEnumerable/WhereNot.cs
+++ src/app/ExtraLINQ/IEnumerable/WhereNot.cs
@@ -22,6 +22,18 @@ namespace ExtraLinq
             return source.Where(item => !predicate(item));
         }
 
+        /// <summary>
+        /// Filters a sequence of values based on a predicate
+        /// and returns those values that don't match the given predicate.
+        /// Each element's index is used in the logic of predicate function.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to filter.</param>
+        /// <param name="predicate">
+        /// A function to test each element for a condition;
+        /// the second parameter of the functions represents the index of the source element.
+        /// </param>
+        /// <returns>Those values that don't match the given predicate.</returns>
         public static IEnumerable<TSource> WhereNot<TSource>(this IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
         {
             ThrowIf.Argument.IsNull(source, "source");
