0219837f8f5732bbbf4c65bb77ff6eace71662f4
Marius Schulz <marius.schulz@me.com>
2015-04-12 14:40:31 +0200

    Renames parameter, adds XML documentation

--- src/app/ExtraLINQ/IEnumerable/Distinct.cs
+++ src/app/ExtraLINQ/IEnumerable/Distinct.cs
@@ -5,30 +5,49 @@ namespace ExtraLinq
 {
     public static partial class EnumerableExtensions
     {
-        public static IEnumerable<TSource> Distinct<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> valueSelector)
+        /// <summary>
+        /// Returns distinct elements from the given sequence using the default equality comparer
+        /// to compare values projected by <paramref name="projection"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the projected value for each element of the sequence.</typeparam>
+        /// <param name="source">The sequence.</param>
+        /// <param name="projection">The projection that is applied to each element to retrieve the value which is being compared.</param>
+        /// <returns>A sequence of elements whose projected values are distinct.</returns>
+        public static IEnumerable<TSource> Distinct<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> projection)
         {
             ThrowIf.Argument.IsNull(source, "source");
-            ThrowIf.Argument.IsNull(valueSelector, "valueSelector");
+            ThrowIf.Argument.IsNull(projection, "projection");
 
-            return DistinctIterator(source, valueSelector, EqualityComparer<TResult>.Default);
+            return DistinctIterator(source, projection, EqualityComparer<TResult>.Default);
         }
 
-        public static IEnumerable<TSource> Distinct<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> valueSelector, IEqualityComparer<TResult> equalityComparer)
+        /// <summary>
+        /// Returns distinct elements from the given sequence using the specified equality comparer
+        /// to compare values projected by <paramref name="projection"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the projected value for each element of the sequence.</typeparam>
+        /// <param name="source">The sequence.</param>
+        /// <param name="projection">The projection that is applied to each element to retrieve the value which is being compared.</param>
+        /// <param name="equalityComparer">The equality comparer to use for comparing the projected values.</param>
+        /// <returns>A sequence of elements whose projected values are considered distinct by the specified equality comparer.</returns>
+        public static IEnumerable<TSource> Distinct<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> projection, IEqualityComparer<TResult> equalityComparer)
         {
             ThrowIf.Argument.IsNull(source, "source");
-            ThrowIf.Argument.IsNull(valueSelector, "valueSelector");
+            ThrowIf.Argument.IsNull(projection, "projection");
             ThrowIf.Argument.IsNull(equalityComparer, "equalityComparer");
 
-            return DistinctIterator(source, valueSelector, equalityComparer);
+            return DistinctIterator(source, projection, equalityComparer);
         }
 
-        private static IEnumerable<TSource> DistinctIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> valueSelector, IEqualityComparer<TResult> equalityComparer)
+        private static IEnumerable<TSource> DistinctIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> projection, IEqualityComparer<TResult> equalityComparer)
         {
             var alreadySeenValues = new HashSet<TResult>(equalityComparer);
 
             foreach (var element in source)
             {
-                var value = valueSelector(element);
+                var value = projection(element);
 
                 if (alreadySeenValues.Contains(value))
                 {
