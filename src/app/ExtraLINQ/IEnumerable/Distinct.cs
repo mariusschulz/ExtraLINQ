7e0437968700dc8e00a628d2483c43e619625ca5
Marius Schulz <marius.schulz@me.com>
2015-04-12 13:41:28 +0200

    Implements Distinct method

--- src/app/ExtraLINQ/IEnumerable/Distinct.cs
+++ src/app/ExtraLINQ/IEnumerable/Distinct.cs
@@ -9,7 +9,25 @@ namespace ExtraLinq
         {
             ThrowIf.Argument.IsNull(source, "source");
 
-            return source;
+            return DistinctIterator(source, valueSelector);
+        }
+
+        private static IEnumerable<TSource> DistinctIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> valueSelector)
+        {
+            var alreadySeenValues = new HashSet<TResult>();
+
+            foreach (var element in source)
+            {
+                var value = valueSelector(element);
+
+                if (alreadySeenValues.Contains(value))
+                {
+                    continue;
+                }
+
+                yield return element;
+                alreadySeenValues.Add(value);
+            }
         }
     }
 }
--- src/tests/ExtraLINQ.Tests/IEnumerable/DistinctTests.cs
+++ src/tests/ExtraLINQ.Tests/IEnumerable/DistinctTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using Xunit;
 
 namespace ExtraLinq.Tests
@@ -12,6 +14,54 @@ namespace ExtraLinq.Tests
             IEnumerable<int> numbers = null;
 
             Assert.Throws<ArgumentNullException>(() => numbers.Distinct(n => n % 2));
+        }
+
+        [Fact]
+        public static void ReturnsAnEmptySequenceWhenSequenceIsEmpty()
+        {
+            int[] numbers = { };
+
+            var distinctNumbers = numbers.Distinct(n => n % 2);
+
+            distinctNumbers.Should().HaveCount(0);
+        }
+
+        [Fact]
+        public static void ReturnsUnmodifiedSequenceIfSequenceHasOneElement()
+        {
+            int[] numbers = { 1 };
+
+            var distinctNumbers = numbers.Distinct(n => n % 2);
+
+            distinctNumbers.Should().Equal(1);
+        }
+
+        [Fact]
+        public static void ReturnsUnmodifiedSequenceIfSequenceHasTwoDifferentElements()
+        {
+            int[] numbers = { 1, 2 };
+
+            var distinctNumbers = numbers.Distinct(n => n % 2);
+
+            distinctNumbers.Should().Equal(1, 2);
+        }
+
+        [Fact]
+        public static void ReturnsOnlyElementsConsideredDistinctByPredicate()
+        {
+            Tuple<int, string>[] digitNames =
+            {
+                Tuple.Create(1, "One"),
+                Tuple.Create(1, "I SHOULDN'T BE HERE"),
+                Tuple.Create(2, "Two"),
+                Tuple.Create(2, "ME NEITHER"),
+                Tuple.Create(2, "ME NEITHER"),
+                Tuple.Create(3, "Three"),
+            };
+
+            var distinctDigitNames = digitNames.Distinct(n => n.Item1);
+
+            distinctDigitNames.Select(t => t.Item2).Should().Equal("One", "Two", "Three");
         }
     }
 }
