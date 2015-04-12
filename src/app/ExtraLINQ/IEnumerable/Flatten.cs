2f8987f547efe67588cb0570d1df0e7f0de7267b
Marius Schulz <marius.schulz@me.com>
2015-04-12 15:57:46 +0200

    Adds XML documentation

--- src/app/ExtraLINQ/IEnumerable/Flatten.cs
+++ src/app/ExtraLINQ/IEnumerable/Flatten.cs
@@ -4,6 +4,12 @@ namespace ExtraLinq
 {
     public static partial class EnumerableExtensions
     {
+        /// <summary>
+        /// Returns a flattened sequence that contains the concatenation of all the nested sequences' elements.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of sequences to be flattened.</param>
+        /// <returns>The concatenation of all the nested sequences' elements.</returns>
         public static IEnumerable<TSource> Flatten<TSource>(this IEnumerable<IEnumerable<TSource>> source)
         {
             ThrowIf.Argument.IsNull(source, "source");
