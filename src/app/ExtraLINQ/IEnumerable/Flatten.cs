0eb25238c6bb839eea00fbb6c5723a00632da9bc
Marius Schulz <marius.schulz@me.com>
2015-04-12 15:51:19 +0200

    Adds tests and implementation

--- src/app/ExtraLINQ/IEnumerable/Flatten.cs
+++ src/app/ExtraLINQ/IEnumerable/Flatten.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace ExtraLinq
 {
@@ -14,7 +13,13 @@ namespace ExtraLinq
 
         private static IEnumerable<TSource> FlattenIterator<TSource>(IEnumerable<IEnumerable<TSource>> source)
         {
-            throw new NotImplementedException();
+            foreach (IEnumerable<TSource> array in source)
+            {
+                foreach (TSource element in array)
+                {
+                    yield return element;
+                }
+            }
         }
     }
 }
--- src/tests/ExtraLINQ.Tests/IEnumerable/FlattenTests.cs
+++ src/tests/ExtraLINQ.Tests/IEnumerable/FlattenTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 
 namespace ExtraLinq.Tests
@@ -11,6 +13,31 @@ namespace ExtraLinq.Tests
             int[][] numbers = null;
 
             Assert.Throws<ArgumentNullException>(() => numbers.Flatten());
+        }
+
+        [Fact]
+        public static void FlattensAnArrayOfArraysOfIntegers()
+        {
+            int[][] numbers =
+            {
+                new[] { 1, 2, 3 },
+                new[] { 4, 5 },
+                new[] { 6 }
+            };
+
+            IEnumerable<int> flattenedNumbers = numbers.Flatten();
+
+            flattenedNumbers.Should().Equal(1, 2, 3, 4, 5, 6);
+        }
+
+        [Fact]
+        public static void ReturnsAnEmptySequenceWhenSequenceIsEmpty()
+        {
+            int[][] numbers = { };
+
+            IEnumerable<int> flattenedNumbers = numbers.Flatten();
+
+            flattenedNumbers.Should().BeEmpty();
         }
     }
 }
