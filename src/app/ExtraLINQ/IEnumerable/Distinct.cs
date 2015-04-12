a08db7683426dae8936d84d0d02e00d4280da857
Marius Schulz <marius.schulz@me.com>
2015-04-12 12:50:28 +0200

    Adds first failing test

---
+++ src/app/ExtraLINQ/IEnumerable/Distinct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtraLinq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static IEnumerable<TSource> Distinct<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> valueSelector)
+        {
+            return source;
+        }
+    }
+}
---
+++ src/tests/ExtraLINQ.Tests/IEnumerable/DistinctTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtraLinq.Tests
+{
+    public class DistinctTests
+    {
+        [Fact]
+        public static void ThrowsArgumentNullExceptionWhenSequenceIsNull()
+        {
+            IEnumerable<int> numbers = null;
+
+            Assert.Throws<ArgumentNullException>(() => numbers.Distinct(n => n % 2));
+        }
+    }
+}
