450250b007b7837ead50500d776a66d0e79041e1
Marius Schulz <marius.schulz@me.com>
2015-04-13 13:34:57 +0200

    Adds two more tests

--- src/tests/ExtraLINQ.Tests/IEnumerable/ChunkTests.cs
+++ src/tests/ExtraLINQ.Tests/IEnumerable/ChunkTests.cs
@@ -53,5 +53,58 @@ namespace ExtraLinq.Tests
             chunks.Should().HaveCount(1);
             chunks.First().Should().Equal(42);
         }
+
+        [Fact]
+        public void CorrectlySplitsASequenceIntoSingleElementChunks()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            int[][] expectedChunks =
+            {
+                new[] { 1 },
+                new[] { 2 },
+                new[] { 3 },
+                new[] { 4 },
+                new[] { 5 },
+                new[] { 6 },
+                new[] { 7 }
+            };
+
+            int[][] actualChunks = numbers.Chunk(1).ToArray();
+
+            actualChunks.Should().HaveSameCount(expectedChunks);
+
+            for (int i = 0; i < actualChunks.Length; i++)
+            {
+                int[] actualChunk = actualChunks[i];
+                int[] expectedChunk = expectedChunks[i];
+
+                actualChunk.Should().Equal(expectedChunk);
+            }
+        }
+
+        [Fact]
+        public void CorrectlySplitsASequenceIntoTwoElementChunks()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            int[][] expectedChunks =
+            {
+                new[] { 1, 2 },
+                new[] { 3, 4 },
+                new[] { 5, 6 },
+                new[] { 7 }
+            };
+
+            int[][] actualChunks = numbers.Chunk(2).ToArray();
+
+            actualChunks.Should().HaveSameCount(expectedChunks);
+
+            for (int i = 0; i < actualChunks.Length; i++)
+            {
+                int[] actualChunk = actualChunks[i];
+                int[] expectedChunk = expectedChunks[i];
+
+                actualChunk.Should().Equal(expectedChunk);
+            }
+        }
     }
 }
