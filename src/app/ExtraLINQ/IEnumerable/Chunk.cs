bf166b4f5dda30a0a568f090b8020a9b13420ab7
Marius Schulz <marius.schulz@me.com>
2015-04-13 12:30:19 +0200

    Adds method and two basic tests

---
+++ src/app/ExtraLINQ/IEnumerable/Chunk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ExtraLinq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static IEnumerable<TSource[]> Chunk<TSource>(this IEnumerable<TSource> source, int chunkLength)
+        {
+            ThrowIf.Argument.IsNull(source, "source");
+
+            return ChunkIterator(source, chunkLength);
+        }
+
+        private static IEnumerable<TSource[]> ChunkIterator<TSource>(IEnumerable<TSource> source, int chunkLength)
+        {
+            yield break;
+        }
+    }
+}
---
+++ src/tests/ExtraLINQ.Tests/IEnumerable/ChunkTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace ExtraLinq.Tests
+{
+    public class ChunkTests
+    {
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenCollectionIsNull()
+        {
+            IEnumerable<object> nullCollection = null;
+
+            Assert.Throws<ArgumentNullException>(() => nullCollection.Chunk(2));
+        }
+
+        [Fact]
+        public void ReturnsAnEmptySequenceWhenPassedAnEmptySequence()
+        {
+            int[] numbers = new int[0];
+
+            IEnumerable<int[]> chunks = numbers.Chunk(2);
+
+            chunks.Should().BeEmpty();
+        }
+    }
+}
