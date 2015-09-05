c4a6c49c2c61cf0eaaf94ffaf24207ce212deeac
Marius Schulz <marius.schulz@me.com>
2015-09-05 19:05:32 +0200

    Documents `TakeSkip` and simplifies implementation

--- src/app/ExtraLINQ/IEnumerable/TakeSkip.cs
+++ src/app/ExtraLINQ/IEnumerable/TakeSkip.cs
@@ -4,6 +4,15 @@ namespace ExtraLinq
 {
     public static partial class EnumerableExtensions
     {
+        /// <summary>
+        /// Iterates over the given sequence and repeatedly returns <paramref name="take"/> elements
+        /// and skips <paramref name="skip"/> elements.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The sequence to iterate over.</param>
+        /// <param name="take">The number of elements to take at each step.</param>
+        /// <param name="skip">The number of elements to skip after each step.</param>
+        /// <returns>A sequence containing the taken elements.</returns>
         public static IEnumerable<TSource> TakeSkip<TSource>(this IEnumerable<TSource> source, int take, int skip)
         {
             ThrowIf.Argument.IsNull(source, "source");
@@ -21,24 +30,16 @@ namespace ExtraLinq
             {
                 for (int i = 0; i < take; i++)
                 {
-                    bool hasNext = enumerator.MoveNext();
-
-                    if (!hasNext)
-                    {
+                    if (!enumerator.MoveNext())
                         yield break;
-                    }
 
                     yield return enumerator.Current;
                 }
 
                 for (int i = 0; i < skip; i++)
                 {
-                    bool hasNext = enumerator.MoveNext();
-
-                    if (!hasNext)
-                    {
+                    if (!enumerator.MoveNext())
                         yield break;
-                    }
                 }
             }
         }
